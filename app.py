9905966f61d70919b49d4e5dfad5bfbecdf40903
srinivas2580devops <83953904+srinivas2580devops@users.noreply.github.com>
2022-06-28 00:05:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return 'Welcome to ZF'
+    return 'Glad to be part of ZF'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
